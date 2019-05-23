d506ab6daaa2bf35a3e9f494e859b359dd057ddd
Jill England <jill.england@comcast.net>
2019-05-22 20:15:06 -0700

    Modify static factbook.json path

--- MergePowerData/MergePowerData/Program.cs
+++ MergePowerData/MergePowerData/Program.cs
@@ -4,6 +4,9 @@
     {
         private static void Main(string[] args)
         {
+            // Currently getting factbook data from Ian's Factbook project
+            // https://github.com/iancoleman/cia_world_factbook_api
+
             var report = new AnalysisCapacity("../../../../../cia_world_factbook_api/data/" + "factbook.json");
             report.ElectricReport();
         }
