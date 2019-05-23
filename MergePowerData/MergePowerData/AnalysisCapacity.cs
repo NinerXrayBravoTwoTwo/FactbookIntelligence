efb63a9cfcfe0d21e067e197fec3151fd14dca2f
Jill England <jill.england@comcast.net>
2019-05-23 00:05:12 -0700

    Refactor for IntelReport class

--- MergePowerData/MergePowerData/AnalysisCapacity.cs
+++ MergePowerData/MergePowerData/AnalysisCapacity.cs
@@ -9,14 +9,8 @@ namespace MergePowerData
 {
     internal class AnalysisCapacity
     {
-        private const double TwentyMtonTnt = 0.93106557; // 20 Mton_e = 0.93106557 kg, ~1 kg
-        private const double TWh2kg = 0.040055402; // TWh = 0.040055402 kg
-        private const double TWh2MTonTnt = 0.86042065; // TWh = 0.86042 M ton Tnt
-        private const double TWh2PJ = 3.6; // TWh = 3.6 PJ (Peta Joule's)
-
         private readonly JObject _fact;
-
-
+        
         public AnalysisCapacity(string fileName)
         {
             _fact = JObject.Parse(File.ReadAllText(fileName));
@@ -26,88 +20,47 @@ namespace MergePowerData
         {
             Console.WriteLine(
                 "ProdTWh\t"
-                //+ "kg_e\t"
+                + "kg_e\t"
                 //+ "pop_M\t"
                 //+ "kWh/pop\t"
                 + "Prod_FF_TWh/y\t"
                 + "Capacity_TWh/y\t"
                 + "Country");
 
-            Electric eWorld = null;
-            AnnualValue2 worldGR = null;
-            AnnualValue3 worldPP = null;
-            InstalledGeneratingCapacity worldIgc = null;
+            var Intel = new Intel();
 
-            var countries =
-                _fact.SelectTokens(
-                    ".countries$..$.data$"); //["data"];//.Where(t => t.SelectToken("name").Value<string>().Equals("World"));
-
-            //foreach (var country in _fact.SelectTokens("countries$..$data"))
             foreach (var item in _fact["countries"])
-            foreach (var country in item)
-            {
-                var data = country["data"];
-                var name = data["name"].Value<string>();
-                //var name2 = data.SelectToken("name").Value<string>();
-
-
-                var energy = data["energy"];
-
-                // population
-                var pop = Population(data);
-
-                if (energy == null) continue;
-
-                var e = energy["electricity"] != null ? new Electric(energy) : null;
-
-                var gdp = Gdp(data);
-
-                AnnualValue2 growthRate;
-                var purchasingPower = EconBase(gdp, out growthRate);
-
-                var igc = e?.Electricity.installed_generating_capacity;
-
-                if (igc == null) continue;
-
-                if (name.Equals("World"))
+                foreach (var country in item)
                 {
-                    eWorld = e;
-                    worldGR = growthRate;
-                    worldPP = purchasingPower;
-                    worldIgc = igc;
+                    var data = country["data"];
+                    var name = data["name"].Value<string>();
+                    //var name2 = data.SelectToken("name").Value<string>();
+
+                    var energy = data["energy"];
+
+                    // population
+                    var pop = Population(data);
+
+                    if (energy == null) continue;
+
+                    var e = energy["electricity"] != null ? new Electric(energy) : null;
+
+                    var gdp = Gdp(data);
+
+                    var purchasingPower = EconBase(gdp, out var growthRate);
+
+                    var igc = e?.Electricity.installed_generating_capacity;
+
+                    if (igc == null) continue;
+
+                    var currentCap = igc.YearCapacityTWhr();
+                    var futureCap50y = igc.YearCapacityTWhr() * 2;
+
+                    Intel.Add(name, e, purchasingPower,  pop);
                 }
 
-                var kgEfossil = e.Electricity.by_source.fossil_fuels.percent / 100 * e.ProdTWh * TWh2kg;
-                var wrldKgEfossil = eWorld.Electricity.by_source.fossil_fuels.percent / 100 * eWorld.ProdTWh * TWh2kg;
+            Intel.Report();
 
-                var currentCap = igc.YearCapacityTWhr();
-                var futureCap50y = igc.YearCapacityTWhr() * 2;
-
-
-                Console.WriteLine(
-                    $"{e.ProdTWh}\t"
-                    + $"{e.ProdTWh * TWh2kg:F1}\t"
-                    //+ $"{pop / 1e6:F0}\t"
-                    //+ $"{e.ProdKWh / pop:F0}\t"
-                    //$"{e.ProdTWh / eWorld.ProdTWh:F3}\t"
-                    //+ $"{kgEfossil / wrldKgEfossil:F3}\t"
-                    // $"{e.TtonCo2 / eWorld.TtonCo2:F3}\t" // want amtCo2 per TW consumed Fossil
-                    //+ $"{growthRate.value:F1}\t"
-                    //+ $"{purchasingPower.value / worldPP.value:F3}\t"
-                    //+ $"{e.Electricity.by_source.nuclear_fuels.percent}\t"
-                    //+ $"{e.Electricity.by_source.hydroelectric_plants.percent}\t"
-                    // + $"{e.Electricity.by_source.fossil_fuels.percent}%\t"
-                    + $"{e.Electricity.by_source.fossil_fuels.percent / 100 * e.ProdTWh:F0}\t"
-                    //+ $"{e.Electricity.by_source.other_renewable_sources.percent}\t"
-                    + $"{igc.YearCapacityTWhr():F0}\t"
-                    //+ $"{igc.YearCapacityTWhr() / e.ProdTWh:F2}\t"
-                    //$"{e.ProdTWh / eWorld.ProdTWh:F3}\t"
-                    //+ $"{kgEfossil / wrldKgEfossil:F3}\t"
-                    // $"{e.TtonCo2 / eWorld.TtonCo2:F3}\t" // want amtCo2 per TW consumed Fossil
-                    //+ $"{growthRate.value:F1}\t"
-                    //+ $"{purchasingPower.value / worldPP.value:F3}\t"
-                    + $"{name}");
-            }
         }
 
         /// <summary>
@@ -128,16 +81,13 @@ namespace MergePowerData
             AnnualValue3 purchasingPower;
             if (gdp.per_capita_purchasing_power_parity == null)
             {
-                purchasingPower = new AnnualValue3 {date = "2000", units = "USD", value = double.NaN};
+                purchasingPower = new AnnualValue3 { date = "2000", units = "USD", value = double.NaN };
             }
             else
             {
                 var pp = gdp.per_capita_purchasing_power_parity.annual_values;
 
-                if (pp != null)
-                    purchasingPower = pp.OrderBy(p => p.date).LastOrDefault();
-                else
-                    purchasingPower = new AnnualValue3 {date = "1990", value = 0, units = "USD"};
+                purchasingPower = pp != null ? pp.OrderBy(p => p.date).LastOrDefault() : new AnnualValue3 { date = "1990", value = 0, units = "USD" };
             }
 
             return purchasingPower;
---
+++ MergePowerData/MergePowerData/Intel.cs
@@ -0,0 +1,84 @@
+﻿
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergePowerData
+{
+    public class Intel
+    {
+        private const double TwentyMtonTnt = 0.93106557; // 20 Mton_e = 0.93106557 kg, ~1 kg
+        private const double TWh2kg = 0.040055402; // TWh = 0.040055402 kg
+        private const double TWh2MTonTnt = 0.86042065; // TWh = 0.86042 M ton Tnt
+        private const double TWh2PJ = 3.6; // TWh = 3.6 PJ (Peta Joule's)
+
+        private List<Country> data = new List<Country>();
+        private Country _world;
+
+        public void Add(string name, Electric electric, AnnualValue3 purchasingPower, long pop)
+        {
+            if (name.Equals("World"))
+                _world = new Country(name, electric, purchasingPower, pop);
+
+
+            data.Add(new Country(name, electric, purchasingPower, pop));
+        }
+
+        internal void Report()
+        {
+            foreach (var c in data.OrderByDescending(d => d.electric.ProdTWh))
+            {
+                                var kgEfossil = c.electric.Electricity.by_source.fossil_fuels.percent / 100 * c.electric.ProdTWh * TWh2kg;
+                var wrldKgEfossil = _world.electric.Electricity.by_source.fossil_fuels.percent / 100 * _world.electric.ProdTWh * TWh2kg;
+
+                var igc = c.electric?.Electricity.installed_generating_capacity;
+
+                if (igc == null) continue;
+
+                var currentCap = igc.YearCapacityTWhr();
+                var futureCap50y = igc.YearCapacityTWhr() * 2;
+
+                Console.WriteLine(
+             $"{c.electric.ProdTWh}\t"
+             + $"{c.electric.ProdTWh * TWh2kg:F1}\t"
+             + $"{c.pop / 1e6:F0}\t"
+             + $"{c.electric.ProdKWh / c.pop:F0}\t"
+             + $"{c.electric.ProdTWh / _world.electric.ProdTWh:F3}\t"
+             + $"{kgEfossil / wrldKgEfossil:F3}\t"
+             + $"{c.electric.TtonCo2 / _world.electric.TtonCo2:F3}\t" // want amtCo2 per TW consumed Fossil
+                                                                      //+ $"{growthRate.value:F1}\t"
+                                                                      //+ $"{purchasingPower.value / worldPP.value:F3}\t"
+             + $"{c.electric.Electricity.by_source.nuclear_fuels.percent}\t"
+             + $"{c.electric.Electricity.by_source.hydroelectric_plants.percent}\t"
+             + $"{c.electric.Electricity.by_source.fossil_fuels.percent}%\t"
+             + $"{c.electric.Electricity.by_source.fossil_fuels.percent / 100 * c.electric.ProdTWh:F0}\t"
+             + $"{c.electric.Electricity.by_source.other_renewable_sources.percent}\t"
+             + $"{igc.YearCapacityTWhr():F0}\t"
+             + $"{igc.YearCapacityTWhr() / c.electric.ProdTWh:F2}\t"
+             + $"{c.electric.ProdTWh / _world.electric.ProdTWh:F3}\t"
+             + $"{kgEfossil / wrldKgEfossil:F3}\t"
+             + $"{c.electric.TtonCo2 / _world.electric.TtonCo2:F3}\t" // want amtCo2 per TW consumed Fossil
+                                                                      //+ $"{growthRate.value:F1}\t"
+                                                                      //+ $"{purchasingPower.value / worldPP.value:F3}\t"
+             + $"{c.name}");
+            }
+        }
+    }
+
+    internal class Country
+    {
+        public readonly string name;
+        public readonly Electric electric;
+        public readonly AnnualValue3 purchasingPower;
+        public readonly long pop;
+
+        public Country(string name, Electric electric, AnnualValue3 purchasingPower, long pop)
+        {
+            this.name = name;
+            this.electric = electric;
+            this.purchasingPower = purchasingPower;
+            this.pop = pop;
+        }
+    }
+}
